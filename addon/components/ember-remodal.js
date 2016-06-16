d656603cbeae95d8299f9e49545c8c8e985ab720
Quincy <qle@ciena.com>
2016-06-16 10:33:52 -0400

    customize remodal

--- addon/components/ember-remodal.js
+++ addon/components/ember-remodal.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import layout from '../templates/components/ember-remodal';
-
 const {
   inject,
   computed,
@@ -8,12 +7,11 @@ const {
   getOwner,
   on,
   RSVP: { Promise },
-  run: { next, scheduleOnce },
+  run: { later, next, scheduleOnce },
   sendEvent,
   warn,
   Component
-} = Ember;
-
+  } = Ember;
 export default Component.extend({
   layout,
   warn,
@@ -37,25 +35,21 @@ export default Component.extend({
   erOpenButton: false,
   erCancelButton: false,
   erConfirmButton: false,
-
   didInsertElement() {
     scheduleOnce('afterRender', this, '_setProperties');
     scheduleOnce('afterRender', this, '_registerObservers');
     scheduleOnce('afterRender', this, '_checkForDeprecations');
     scheduleOnce('afterRender', this, '_checkForTestingEnv');
   },
-
   willDestroyElement() {
     scheduleOnce('destroy', this, '_destroyDomElements');
     scheduleOnce('destroy', this, '_deregisterObservers');
   },
-
   modalId: computed('elementId', {
     get() {
       return `[data-remodal-id=${this.get('elementId')}]`;
     }
   }),
-
   animationState: computed('disableAnimation', {
     get() {
       if (this.get('disableAnimation')) {
@@ -65,19 +59,15 @@ export default Component.extend({
       }
     }
   }),
-
   openDidFire: on('opened', function() {
     this.sendAction('onOpen');
   }),
-
   closeDidFire: on('closed', function() {
     this.sendAction('onClose');
   }),
-
   open() {
     return this._promiseAction('open');
   },
-
   close() {
     if (this.get('modal')) {
       return this._promiseAction('close');
@@ -89,18 +79,14 @@ export default Component.extend({
       return new Promise((resolve) => resolve(this));
     }
   },
-
   _promiseAction(action) {
     let modal = this.get('modalId');
     let actionName = this._pastTense(action);
-
     this.send(action);
-
     return new Promise((resolve) => {
       Ember.$(document).one(actionName, modal, () => resolve(this));
     });
   },
-
   _pastTense(action) {
     if (action[action.length - 1] === 'e') {
       return `${action}d`;
@@ -108,39 +94,32 @@ export default Component.extend({
       return `${action}ed`;
     }
   },
-
   _setProperties() {
     let opts = this.get('options');
-
     if (opts) {
       this.setProperties(opts);
     }
-
     if (this.get('forService')) {
       this.get('remodal').set(this.get('name'), this);
     }
   },
-
   _registerObservers() {
     let modal = this.get('modalId');
     Ember.$(document).on('opened', modal, () => sendEvent(this, 'opened'));
     Ember.$(document).on('closed', modal, () => sendEvent(this, 'closed'));
   },
-
   _deregisterObservers() {
     let modal = this.get('modalId');
     Ember.$(document).off('opened', modal);
     Ember.$(document).off('closed', modal);
   },
-
   _destroyDomElements() {
     const modal = this.get('modal');
-
     if (modal) {
       modal.destroy();
+      delete this.modal;
     }
   },
-
   _createInstanceAndOpen() {
     let modal = Ember.$(this.get('modalId')).remodal({
       hashTracking: this.get('hashTracking'),
@@ -148,71 +127,62 @@ export default Component.extend({
       closeOnEscape: this.get('closeOnEscape'),
       modifier: this.get('modifier')
     });
-
     this.set('modal', modal);
     this.send('open');
   },
-
   _checkForDeprecations() {
     // Deprecations go here
   },
-
   _checkForTestingEnv() {
     let config = this._getConfig();
-
     if (config) {
       let env = config.environment;
       let remodalConfig = config['ember-remodal'];
       let disableAnimation;
-
       if (remodalConfig) {
         disableAnimation = remodalConfig.disableAnimationWhileTesting;
       }
-
       if (disableAnimation && env === 'test') {
         this.set('disableAnimation', true);
       }
     }
   },
-
   _getConfig() {
     return getOwner(this).resolveRegistration('config:environment');
   },
-
   _openModal() {
     this.get('modal').open();
   },
-
   _closeModal() {
     this.get('modal').close();
+    later(this, '_destroyDomElements', 500);
   },
-
   _closeOnCondition(condition) {
     this.sendAction(`on${condition}`);
-
     if (this.get(`closeOn${condition}`)) {
       this.send('close');
     }
   },
-
+  _toggleOpen() {
+    this.toggleProperty('isOpen')
+  },
   actions: {
     confirm() {
       this._closeOnCondition('Confirm');
     },
-
     cancel() {
       this._closeOnCondition('Cancel');
     },
-
     open() {
+      this.set('isOpen', true)
       if (this.get('modal')) {
         scheduleOnce('afterRender', this, '_openModal');
       } else {
         scheduleOnce('afterRender', this, '_createInstanceAndOpen');
       }
     },
-
     close() {
+      later(this, '_toggleOpen', 500);
       next(this, '_closeModal');
     }
   }
